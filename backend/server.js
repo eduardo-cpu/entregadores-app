1d7f1cb1f23992b52af33e4432294fdaf82e7e6e
Eduardo <eduardosantossm@hotmail.com>
2025-04-29 20:03:45 -0300

    🐛 fix: Ajusta servidor e configuração para deploy no Render

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 
 // Conexão com o MongoDB
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/entregadores-app';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://espaula:jfGqI8wSqsSBjBSf@entregadores.ilashuq.mongodb.net/entregadores-app?retryWrites=true&w=majority&appName=Entregadores';
 
 // Supressão do aviso de deprecation
 mongoose.set('strictQuery', false);
